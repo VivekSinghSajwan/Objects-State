d0563494cf9198f1f90f38ed13cb6671b8cf893a
VivekSinghSajwan <vssajwan09123@gmail.com>
2025-04-20 17:39:23 +0530

    generate multiple components

--- src/LudoTab.jsx
+++ src/LudoTab.jsx
@@ -1,49 +1,23 @@
 import { useState } from "react";
+import LudoBlock from "./LudoBlock";
 
 function LudoTab(){
 
     let [moves , setMoves] = useState({blue:0,yellow:0,red:0,green:0})
 
-    function blueIsClicked(){
+    function incrementMove(color){
         let copyObj = {...moves};
-        copyObj.blue += 1
-        setMoves(copyObj);
-    }
-    function yellowIsClicked(){
-        let copyObj = {...moves};
-        copyObj.yellow += 1
-        setMoves(copyObj);
-    }
-    function redIsClicked(){
-        let copyObj = {...moves};
-        copyObj.red += 1
-        setMoves(copyObj);
-    }
-    function greenIsClicked(){
-        let copyObj = {...moves};
-        copyObj.green += 1
+        copyObj[color] += 1
         setMoves(copyObj);
     }
 
     return (
         <div>
             <p>Game Begins</p>
-            <div className="blue">
-                <p>Blue Moves: {moves.blue}</p>
-                <button onClick={blueIsClicked} style={{backgroundColor: "blue"}}>+1</button>
-            </div>
-            <div className="yellow">
-                <p>Yellow Moves: {moves.yellow}</p>
-                <button onClick={yellowIsClicked} style={{backgroundColor: "orange"}}>+1</button>
-            </div>
-            <div className="red">
-                <p>Red Moves: {moves.red}</p>
-                <button onClick={redIsClicked} style={{backgroundColor: "red"}}>+1</button>
-            </div>
-            <div className="green">
-                <p>Green Moves: {moves.green}</p>
-                <button onClick={greenIsClicked} style={{backgroundColor: "green"}}>+1</button>
-            </div>
+            <LudoBlock color="blue" moves={moves.blue} onClick={()=>incrementMove("blue")}/>
+            <LudoBlock color="orange" moves={moves.yellow} onClick={()=>incrementMove("yellow")}/>
+            <LudoBlock color="red" moves={moves.red} onClick={()=>incrementMove("red")}/>
+            <LudoBlock color="green" moves={moves.green} onClick={()=>incrementMove("green")}/>
         </div>
     )
 }
